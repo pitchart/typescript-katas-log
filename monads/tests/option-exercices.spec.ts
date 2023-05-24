2adfe9d115616be9691b608e1b5e7c7a20c8b7b0
alexandrerousselledecath <alexandre.rousselle@decathlon.com>
2023-05-24 11:20:57 +0200

    chore: add tests

--- monads/tests/option-exercices.spec.ts
+++ monads/tests/option-exercices.spec.ts
@@ -1,97 +1,95 @@
-import {None, NoSuchElementError, Option, Some} from "funfix-core"
-import {Person} from "../src/persons/person";
-import {people} from "./fixtures/persons.fixtures";
+import { None, NoSuchElementError, Option, Some } from 'funfix-core'
+import { Person } from '../src/persons/person'
+import { people } from './fixtures/persons.fixtures'
 
 describe('Option exercices', () => {
+  test('Filter a list of persons', () => {
+    const persons: Array<Option<string>> = [None, Some('john doe'), Some('Mary Smith'), None]
 
-    test('Filter a list of persons', () => {
-        const persons: Option<string>[] = [None, Some("john doe"), Some("Mary Smith"), None]
+    const definedPersons: string[] = persons.filter(o => !o.isEmpty()).map(o => o.get())
 
-        const definedPersons: string[] = persons.filter(o => !o.isEmpty()).map(o => o.get());
+    expect(definedPersons).toHaveLength(2)
+  })
 
-        expect(definedPersons).toHaveLength(2)
-    })
+  test('working with null', () => {
+    // Instantiate a None Option of string
+    // map it to an Upper case function
+    // then it must return the string "Ich bin empty" if empty
+    const iAmAnOption: Option<string> = None
+    const optionValue: string = iAmAnOption.map(s => s.toUpperCase()).getOrElse('Ich bin empty')
 
-    test("working with null", () => {
-        // Instantiate a None Option of string
-        // map it to an Upper case function
-        // then it must return the string "Ich bin empty" if empty
-        const iAmAnOption: Option<string> = None;
-        const optionValue: string = iAmAnOption.map(s => s.toUpperCase()).getOrElse("Ich bin empty")
+    expect(iAmAnOption.isEmpty()).toBeTruthy()
+    expect(optionValue).toBe('Ich bin empty')
+  })
 
-        expect(iAmAnOption.isEmpty()).toBeTruthy();
-        expect(optionValue).toBe("Ich bin empty");
-    })
+  test('find Karadoc', () => {
+    // Find Karadoc in the people List or return Perceval
+    const foundPersonByLastName = (name: string): Option<string> => {
+      return Some(new Person('Provençal', 'Le Gaulois').fullName())
+        .filter(fullName => {
+          return fullName == name
+        })
+    }
 
-    test("find Karadoc", () => {
-        // Find Karadoc in the people List or return Perceval
-        const foundPersonByLastName = (name: string): Option<string> => {
+    expect(foundPersonByLastName('Karadoc').getOrElse('Perceval')).toBe('Perceval')
+  })
 
-            return Some(new Person('Provençal', 'Le Gaulois').fullName())
-                .filter(fullName => {
-                    return fullName == name;
-                });
-        };
+  test('find a person or die trying', () => {
+    // Find a person matching firstName and lastName
+    // throw a NoSuchElementException if not found
+    const firstName = 'Rick'
+    const lastName = 'Sanchez'
 
-        expect(foundPersonByLastName('Karadoc').getOrElse('Perceval')).toBe("Perceval");
-    })
+    const findPersonOrDieTrying = () => Option
+      .of(people
+        .find(p => p.fullName() == `${firstName} ${lastName}`))
 
-    test("find a person or die trying", () => {
-        // Find a person matching firstName and lastName
-        // throw a NoSuchElementException if not found
-        const firstName = "Rick";
-        const lastName = "Sanchez";
+      .getOrElseL(() => {
+        throw new NoSuchElementError('miss')
+      })
 
-        const findPersonOrDieTrying = () => Option
-            .of(people
-                .find(p => p.fullName() == `${firstName} ${lastName}`))
+    expect(findPersonOrDieTrying).toThrow(NoSuchElementError)
+  })
 
-            .getOrElseL(() => {
-                throw new NoSuchElementError("miss");
-            });
+  test('chain call', () => {
+    // Chain calls to the half method 4 times with start in argument
+    // For each half append the value to the concat variable (side effect)
+    let j = 0
+    const half = (x: number): Option<number> => {
+      j++
+      return x % 2 == 0 ? Some(x / 2) : None
+    }
 
-        expect(findPersonOrDieTrying).toThrow(NoSuchElementError);
-    })
+    const start: number = 500
 
-    test("chain call", () => {
-        // Chain calls to the half method 4 times with start in argument
-        // For each half append the value to the concat variable (side effect)
-        let j= 0;
-        const half = (x: number): Option<number> => {
-            j++;
-            return x % 2 == 0 ? Some(x / 2) : None
-        };
+    let concatenation = ''
+    let i = 0
 
-        const start: number = 500;
+    const result = Some(start)
+      .chain(half).ap(Option.of((num) => {
+        i++
+        concatenation += num
+        return num
+      }))
+      .chain(half).ap(Option.of((num) => {
+        i++
+        concatenation += num
+        return num
+      }))
+      .chain(half).ap(Option.of((num) => {
+        i++
+        concatenation += num
+        return num
+      }))
+      .chain(half).ap(Option.of((num) => {
+        i++
+        concatenation += num
+        return num
+      }))
 
-        let concatenation = "";
-        let i= 0;
-
-        const result = Some(start)
-            .chain(half).ap(Option.of((num) => {
-                i++;
-                concatenation += num;
-                return num;
-            }))
-            .chain(half).ap(Option.of((num) => {
-                i++;
-                concatenation += num;
-                return num;
-            }))
-            .chain(half).ap(Option.of((num) => {
-                i++;
-                concatenation += num;
-                return num;
-            }))
-            .chain(half).ap(Option.of((num) => {
-                i++;
-                concatenation += num;
-                return num;
-            }));
-
-        expect(result.isEmpty()).toBeTruthy();
-        expect(concatenation).toBe("250125");
-        expect(i).toBe(2);
-        expect(j).toBe(3);
-    })
+    expect(result.isEmpty()).toBeTruthy()
+    expect(concatenation).toBe('250125')
+    expect(i).toBe(2)
+    expect(j).toBe(3)
+  })
 })
